57244614159f18060c6ecc3bd6e426c8d271fd48
Dolly <dollysamuel.official@gmail.com>
2024-09-09 11:50:30 -0400

    ggit

---
+++ src/components/card.test.jsx
@@ -0,0 +1,25 @@
+import {render, screen} from '@testing-library/react'
+import Card from './Card';
+
+describe('Card component', () => {
+
+test('renders cardText', () => {
+    render(<Card />);
+    const imageElement = screen.getByAltText('profile picture');
+    expect(imageElement).toBeInTheDocument();
+  });
+  
+  it('render h2 element', () => {
+    render(<Card  title ="Minion" text ="I am a Banana"/>);
+    const titleElement = screen.getByText(/Minion/i)
+    expect(titleElement).toBeInTheDocument()
+  })
+
+  it('render paragraph text', () => {
+
+    render(<Card title ="Minion" text ="I am a Banana"/>);
+    const paragraphData = screen.getByText(/I am a Banana/i)
+    expect(paragraphData).toBeInTheDocument()
+  })
+  
+})
