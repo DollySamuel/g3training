0b94baa4cd5bd9d763efa452e4581d5452d18faa
Dolly <dollysamuel.official@gmail.com>
2024-09-09 12:04:00 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Card from './components/Card.jsx';
+
+function App() {
+  return (
+    <div>
+      <Card title ="Hello" text ="I am a Banana"/>
+    </div>
+  );
+}
+
+export default App;
