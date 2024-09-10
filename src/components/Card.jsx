886176411378a7c360a77b9ae540f3c1d3349c2c
Dolly <dollysamuel.official@gmail.com>
2024-09-10 18:00:57 -0400

    creating multiple cards using same component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import minionImage from '../assets/minion.jpg'
 
-export default function Card(props) {
+export default function Card() {
+
+  const [cards] = useState([
+    {
+      altText : "Card one",
+      title : "Minion",
+      text : "I am a Minion"
+    },
+    {
+      altText : "Card two",
+      title : "Margo",
+      text : "I am a Margo"
+    },
+    {
+      altText : "Card three",
+      title : "Mona",
+      text : "I am Mona"
+    },
+
+  ])
   return (
-    <div className="card">
-        <img className="card-image" src={minionImage} alt="profile picture"></img>
+    <div >
+        {
+          cards.map((card, i)=> (
+            <div className="card" key= {i}>
+            <img className="card-image" src={minionImage} alt={card.altText}></img>
 
-        <h2 className="card-title">{props.title}</h2>
+            <h2 className="card-title">{card.title}</h2>
 
-        <p className="card-text">{props.text}</p>
+            <p className="card-text">{card.text}</p>
+            </div>
+          ))
+        }
+        
 
     </div> 
   )
